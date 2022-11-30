e153e5973133eb9fb5b95e9c2d4e43238352ddd5
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-11-30 13:04:08 +0100

    promena u prikazu datuma

--- script.js
+++ script.js
@@ -208,9 +208,9 @@ btnLogin.addEventListener('click', function (e) {
       hour: 'numeric',
       minute: 'numeric',
       day: 'numeric',
-      month: 'long',
+      month: 'numeric',
       year: 'numeric',
-      weekday: 'long',
+      // weekday: 'long',
     };
     const locale = navigator.language;
 
